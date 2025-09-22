406065097e130f2c6b34943024121833111fe581
AMIT KUMAR <111384406+amit864kum@users.noreply.github.com>
2025-09-22 10:28:47 +0530

    Update fl_node.py

--- fl_node.py
+++ fl_node.py
@@ -5,7 +5,6 @@ import torch.nn.functional as F
 class SimpleAudioClassifier(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(SimpleAudioClassifier, self).__init__()
-        # Input shape for Conv1d: [batch_size, channels, length]
         self.conv1 = nn.Conv1d(in_channels=input_dim, out_channels=128, kernel_size=3)
         self.relu = nn.ReLU()
         self.pool = nn.AdaptiveAvgPool1d(1)
@@ -14,6 +13,7 @@ class SimpleAudioClassifier(nn.Module):
     def forward(self, x):
         x = self.conv1(x)
         x = self.relu(x)
-        x = x.view(x.size(0), -1)
+        x = self.pool(x)
+        x = torch.flatten(x, 1)
         x = self.fc(x)
         return x
